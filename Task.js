21b886931d87b63914d6c76c47e6d74e81df762a
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2016-03-29 08:15:48 +0900

    upload to gh-pages

---
+++ Task.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default class Task extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDestroy = this.handleDestroy.bind(this);
+    this.handleUpdate = this.handleUpdate.bind(this);
+  }
+
+  handleDestroy(e) {
+    e.preventDefault();
+    this.props.onTaskDestroy(this.props.id);
+  }
+
+  handleUpdate(e) {
+    e.preventDefault();
+    this.props.onTaskUpdate({task: {id: this.props.id, status: e.target.value}});
+  }
+
+  render() {
+    return (
+      <tr key={this.props.id}>
+        <td>
+          {this.props.content}
+        </td>
+        <td>
+          <select className="form-control" defaultValue={this.props.status} onChange={this.handleUpdate} >
+            <option value="todo" key="todo">todo</option>
+            <option value="doing" key="doing">doing</option>
+            <option value="done" key="done">done</option>
+          </select>
+        </td>
+        <td>
+          <button className="btn btn-danger" onClick={this.handleDestroy}>destroy</button>
+        </td>
+      </tr>
+    );
+  }
+}
---
+++ app.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TaskApp from "./TaskApp";
+
+ReactDOM.render(
+  <TaskApp url="http://localhost:3000/tasks" pollInterval={2000} />,
+  document.getElementById('container')
+);
